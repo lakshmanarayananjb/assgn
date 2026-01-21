823f9d841fb160b2327654148d57683c65dedc34
EuniceJoshi <eunicejoshiap@yantravision.com>
2026-01-21 15:10:11 +0530

    updated laksh rep

--- main.c
+++ main.c
@@ -1,26 +1,31 @@
-
 #include <stdio.h>
 
-/* Function declarations */
 int sub(int a, int b);
+int add(int a, int b);
 int mul(int a, int b);
+int div(int a, int b);
 
-/* main function */
 int main() {
     int x = 10, y = 5;
 
     printf("Subtraction: %d\n", sub(x, y));
+    printf("Addition: %d\n", add(x, y));
     printf("Multiplication: %d\n", mul(x, y));
-
+    printf("division: %d\n", div(x, y));
     return 0;
 }
 
-/* sub function */
 int sub(int a, int b) {
     return a - b;
 }
 
-/* mul function */
+int add(int a, int b) {
+    return a + b;
+}
 int mul(int a, int b) {
     return a * b;
+}
+
+int div(int a, int b) {
+    return a / b;
 }
